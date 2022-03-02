f45f4be98623e0992b67483938090188582d8ad2
Jehad91 <j.abushaqra1@gmail.com>
2022-03-02 17:03:23 +0200

    Fixed add player file Relates #24

--- public/js/showPlayers.js
+++ public/js/showPlayers.js
@@ -18,7 +18,7 @@ const showPlayers = (data) => {
           'plus-icon',
           '',
           'src',
-          'img/plus.png'
+          'img/plus.png',
         );
         const nums = createElement('ul', 'nums', '');
         for (let i = 0; i <= 10; i += 1) {
@@ -37,27 +37,27 @@ const showPlayers = (data) => {
         const playerDetails1 = createElement(
           'p',
           'player-details',
-          `Nationality: ${e.nationality}`
+          `Nationality: ${e.nationality}`,
         );
         const playerDetails2 = createElement(
           'p',
           'player-details',
-          `Birth Date: ${e.birthdate}`
+          `Birth Date: ${e.birthdate}`,
         );
         const playerDetails3 = createElement(
           'p',
           'player-details',
-          `Birth Country: ${e.birthcountry}`
+          `Birth Country: ${e.birthcountry}`,
         );
         const playerDetails4 = createElement(
           'p',
           'player-details',
-          `Height: ${e.height}`
+          `Height: ${e.height}`,
         );
         const playerDetails5 = createElement(
           'p',
           'player-details',
-          `Weight: ${e.weight}`
+          `Weight: ${e.weight}`,
         );
         divBtn.append(plusIcon, nums);
         playerInfo.append(
@@ -66,7 +66,7 @@ const showPlayers = (data) => {
           playerDetails2,
           playerDetails3,
           playerDetails4,
-          playerDetails5
+          playerDetails5,
         );
         playerCard.append(divBtn, playerImage, playerInfo);
         playerContainer.appendChild(playerCard);
